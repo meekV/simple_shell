15c0d2d3ce0aedf0eeeb4858a6fd681298593fd8
MeekV <vainqueurmeek@gmail.com>
2023-12-12 15:23:21 -0800

    beautiful code

---
+++ oddeven.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		if (i % 2 == 0)
+		{
+			printf("%d is even\n", i);
+		}
+		else
+		{,
+			printf("d is odd\n", i);
+		}
+	}
+
+	return (0);
+}
