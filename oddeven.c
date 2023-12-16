406d3477b2ea83d414550e51e28be7d0e225e215
MeekV <vainqueurmeek@gmail.com>
2023-12-16 01:13:03 -0800

    Betty would be proud

--- oddeven.c
+++ oddeven.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        if (i % 2 == 0)
-        {
-            printf("%d is even\n", i);
-        }
-        else
-        {
-            printf("%d is odd\n", i);
-        }
-    }
+	for (i = 0; i < 10; i++)
+	{
+		if (i % 2 == 0)
+		{
+			printf("%d is even\n", i);
+		}
+		else
+		{
+			printf("%d is odd\n", i);
+		}
+	}
 
-    return (0);
+	return (0);
 }
